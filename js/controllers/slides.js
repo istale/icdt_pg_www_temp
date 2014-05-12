4f52d7f272e54b4d54d814c720a9d8320f2b8e78
istale <iscarryon@gmail.com>
2014-05-12 11:44:34 +0800

    initial commit

---
+++ js/controllers/slides.js
@@ -0,0 +1,19 @@
+﻿
+/*http://codepen.io/ionic/pen/AjgEB*/
+
+app.controller('SlidesCtrl', ['$scope', '$state', '$$ionicSlideBoxDelegate', function ($scope, $state, $ionicSlideBoxDelegate) {
+
+    $scope.next = function () {
+        $ionicSlideBoxDelegate.next();
+    };
+    $scope.previous = function () {
+        $ionicSlideBoxDelegate.previous();
+    };
+
+    // Called each time the slide changes
+    $scope.slideChanged = function (index) {
+        $scope.slideIndex = index;
+    };
+}]);
+
+
