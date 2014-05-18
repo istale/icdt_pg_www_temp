2e516d3ed32f93f0f96178e353120c0af2272633
istale <iscarryon@gmail.com>
2014-05-18 21:46:47 +0800

    kevin: test if gcm has feedback reg id v5 by weinre on real device v6

--- js/services/push_plugin.js
+++ js/services/push_plugin.js
@@ -1,6 +1,6 @@
 ﻿//factory for processing push notifications.
 
-app.factory('PushProcessingService', function ($state) {
+app.factory('PushProcessingService', function () {
     function onDeviceReady() {
            console.info('NOTIFY  Device is ready.  Registering with GCM server');
            //register with google GCM server
@@ -15,69 +15,6 @@ app.factory('PushProcessingService', function ($state) {
        function gcmErrorHandler(error) {
            console.error('NOTIFY  ' + error);
        }
-
-    // ALL GCM notifications come through here. 
-       function onNotificationGCM(e) {
-           console.log('EVENT -> RECEIVED:' + e.event + '');
-           switch (e.event) {
-               case 'registered':
-                   if (e.regid.length > 0) {
-                       console.log('REGISTERED with GCM Server -> REGID:' + e.regid + "");
-                       //alert('REGISTERED with GCM Server -> REGID:' + e.regid + "");
-
-                       //call back to web service in Angular.  
-                       //This works for me because in my code I have a factory called
-                       //      PushProcessingService with method registerID
-                       var elem = angular.element(document.querySelector('[ng-app]'));
-                       var injector = elem.injector();
-                       var myService = injector.get('PushProcessingService');
-                       myService.registerID(e.regid);
-                   }
-                   break;
-
-               case 'message':
-                   // if this flag is set, this notification happened while we were in the foreground.
-                   // you might want to play a sound to get the user's attention, throw up a dialog, etc.
-                   if (e.foreground) {
-                       //we're using the app when a message is received.
-                       console.log('--INLINE NOTIFICATION--' + '');
-
-                       // if the notification contains a soundname, play it.
-                       //var my_media = new Media("/android_asset/www/"+e.soundname);
-                       //my_media.play();
-                       alert(e.message);
-                   }
-                   else {
-                       // otherwise we were launched because the user touched a notification in the notification tray.
-                       if (e.coldstart)
-                           console.log('--COLDSTART NOTIFICATION--' + '');
-                       else
-                           console.log('--BACKGROUND NOTIFICATION--' + '');
-
-                       // direct user here:
-                       //window.location = "#/app";
-                       $state.transitionTo('app.playlists');
-
-                   }
-
-                   console.log('MESSAGE -> MSG: ' + e.payload.message + '');
-                   console.log('MESSAGE: ' + JSON.stringify(e.payload));
-                   break;
-
-               case 'error':
-                   console.log('ERROR -> MSG:' + e.msg + '');
-                   break;
-
-               default:
-                   console.log('EVENT -> Unknown, an event was received and we do not know what it is');
-                   break;
-           }
-       }
-
-
-
-
-
 
        return {
            initialize: function () {
@@ -114,3 +51,60 @@ app.factory('PushProcessingService', function ($state) {
    });
 
 
+// ALL GCM notifications come through here. 
+function onNotificationGCM(e) {
+    console.log('EVENT -> RECEIVED:' + e.event + '');
+    switch (e.event) {
+        case 'registered':
+            if (e.regid.length > 0) {
+                console.log('REGISTERED with GCM Server -> REGID:' + e.regid + "");
+                //alert('REGISTERED with GCM Server -> REGID:' + e.regid + "");
+
+                //call back to web service in Angular.  
+                //This works for me because in my code I have a factory called
+                //      PushProcessingService with method registerID
+                var elem = angular.element(document.querySelector('[ng-app]'));
+                var injector = elem.injector();
+                var myService = injector.get('PushProcessingService');
+                myService.registerID(e.regid);
+            }
+            break;
+
+        case 'message':
+            // if this flag is set, this notification happened while we were in the foreground.
+            // you might want to play a sound to get the user's attention, throw up a dialog, etc.
+            if (e.foreground) {
+                //we're using the app when a message is received.
+                console.log('--INLINE NOTIFICATION--' + '');
+
+                // if the notification contains a soundname, play it.
+                //var my_media = new Media("/android_asset/www/"+e.soundname);
+                //my_media.play();
+                alert(e.message);
+            }
+            else {
+                // otherwise we were launched because the user touched a notification in the notification tray.
+                if (e.coldstart)
+                    console.log('--COLDSTART NOTIFICATION--' + '');
+                else
+                    console.log('--BACKGROUND NOTIFICATION--' + '');
+
+                // direct user here:
+                window.location = "#/playlists";
+                
+
+            }
+
+            console.log('MESSAGE -> MSG: ' + e.payload.message + '');
+            console.log('MESSAGE: ' + JSON.stringify(e.payload));
+            break;
+
+        case 'error':
+            console.log('ERROR -> MSG:' + e.msg + '');
+            break;
+
+        default:
+            console.log('EVENT -> Unknown, an event was received and we do not know what it is');
+            break;
+    }
+}
