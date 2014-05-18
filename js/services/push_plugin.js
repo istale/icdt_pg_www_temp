74805ae8ae4680f0104c3c81dd842f78887fc8b3
istale <iscarryon@gmail.com>
2014-05-18 22:23:59 +0800

    kevin: test if gcm has feedback reg id v5 by weinre on real device v9

--- js/services/push_plugin.js
+++ js/services/push_plugin.js
@@ -7,71 +7,13 @@ app.factory('PushProcessingService', function ($state) {
            var pushNotification = window.plugins.pushNotification;
 
            // kevin: senderID as google api project number
-           pushNotification.register(gcmSuccessHandler, gcmErrorHandler, { "senderID": "191058528719", "ecb": "PushProcessingService.onNotificationGCM" });
+           pushNotification.register(gcmSuccessHandler, gcmErrorHandler, { "senderID": "191058528719", "ecb": "onNotificationGCM" });
        }
        function gcmSuccessHandler(result) {
            console.info('NOTIFY  pushNotification.register succeeded.  Result = ' + result)
        }
        function gcmErrorHandler(error) {
            console.error('NOTIFY  ' + error);
-       }
-
-       // ALL GCM notifications come through here. 
-       function onNotificationGCM(e) {
-           console.log('EVENT -> RECEIVED:' + e.event + '');
-           switch (e.event) {
-               case 'registered':
-                   if (e.regid.length > 0) {
-                       console.log('REGISTERED with GCM Server -> REGID:' + e.regid + "");
-                       //alert('REGISTERED with GCM Server -> REGID:' + e.regid + "");
-
-                       //call back to web service in Angular.  
-                       //This works for me because in my code I have a factory called
-                       //      PushProcessingService with method registerID
-                       var elem = angular.element(document.querySelector('[ng-app]'));
-                       var injector = elem.injector();
-                       var myService = injector.get('PushProcessingService');
-                       myService.registerID(e.regid);
-                   }
-                   break;
-
-               case 'message':
-                   // if this flag is set, this notification happened while we were in the foreground.
-                   // you might want to play a sound to get the user's attention, throw up a dialog, etc.
-                   if (e.foreground) {
-                       //we're using the app when a message is received.
-                       console.log('--INLINE NOTIFICATION--' + '');
-
-                       // if the notification contains a soundname, play it.
-                       //var my_media = new Media("/android_asset/www/"+e.soundname);
-                       //my_media.play();
-                       alert(e.message);
-                   }
-                   else {
-                       // otherwise we were launched because the user touched a notification in the notification tray.
-                       if (e.coldstart)
-                           console.log('--COLDSTART NOTIFICATION--' + '');
-                       else
-                           console.log('--BACKGROUND NOTIFICATION--' + '');
-
-                       // direct user here:
-                       //window.location = "#/playlists";
-                       $state.transitionTo('app.playlists');
-
-                   }
-
-                   console.log('MESSAGE -> MSG: ' + e.payload.message + '');
-                   console.log('MESSAGE: ' + JSON.stringify(e.payload));
-                   break;
-
-               case 'error':
-                   console.log('ERROR -> MSG:' + e.msg + '');
-                   break;
-
-               default:
-                   console.log('EVENT -> Unknown, an event was received and we do not know what it is');
-                   break;
-           }
        }
 
        return {
@@ -104,66 +46,63 @@ app.factory('PushProcessingService', function ($state) {
                        console.info('unregister success')
                    });
                }
-           },
-           onNotificationGCM: onNotificationGCM
+           }
        }
    });
 
 
 // ALL GCM notifications come through here. 
-//function onNotificationGCM(e) {
-//    console.log('EVENT -> RECEIVED:' + e.event + '');
-//    switch (e.event) {
-//        case 'registered':
-//            if (e.regid.length > 0) {
-//                console.log('REGISTERED with GCM Server -> REGID:' + e.regid + "");
-//                //alert('REGISTERED with GCM Server -> REGID:' + e.regid + "");
+function onNotificationGCM(e) {
+    console.log('EVENT -> RECEIVED:' + e.event + '');
+    switch (e.event) {
+        case 'registered':
+            if (e.regid.length > 0) {
+                console.log('REGISTERED with GCM Server -> REGID:' + e.regid + "");
+                //alert('REGISTERED with GCM Server -> REGID:' + e.regid + "");
 
-//                //call back to web service in Angular.  
-//                //This works for me because in my code I have a factory called
-//                //      PushProcessingService with method registerID
-//                var elem = angular.element(document.querySelector('[ng-app]'));
-//                var injector = elem.injector();
-//                var myService = injector.get('PushProcessingService');
-//                myService.registerID(e.regid);
-//            }
-//            break;
+                //call back to web service in Angular.  
+                //This works for me because in my code I have a factory called
+                ////      PushProcessingService with method registerID
+                //var elem = angular.element(document.querySelector('[ng-app]'));
+                //var injector = elem.injector();
+                //var myService = injector.get('PushProcessingService');
+                //myService.registerID(e.regid);
+            }
+            break;
 
-//        case 'message':
-//            // if this flag is set, this notification happened while we were in the foreground.
-//            // you might want to play a sound to get the user's attention, throw up a dialog, etc.
-//            if (e.foreground) {
-//                //we're using the app when a message is received.
-//                console.log('--INLINE NOTIFICATION--' + '');
+        case 'message':
+            // if this flag is set, this notification happened while we were in the foreground.
+            // you might want to play a sound to get the user's attention, throw up a dialog, etc.
+            if (e.foreground) {
+                //we're using the app when a message is received.
+                console.log('--INLINE NOTIFICATION--' + '');
 
-//                // if the notification contains a soundname, play it.
-//                //var my_media = new Media("/android_asset/www/"+e.soundname);
-//                //my_media.play();
-//                alert(e.message);
-//            }
-//            else {
-//                // otherwise we were launched because the user touched a notification in the notification tray.
-//                if (e.coldstart)
-//                    console.log('--COLDSTART NOTIFICATION--' + '');
-//                else
-//                    console.log('--BACKGROUND NOTIFICATION--' + '');
+                // if the notification contains a soundname, play it.
+                //var my_media = new Media("/android_asset/www/"+e.soundname);
+                //my_media.play();
+                alert(e.message);
+            }
+            else {
+                // otherwise we were launched because the user touched a notification in the notification tray.
+                if (e.coldstart)
+                    console.log('--COLDSTART NOTIFICATION--' + '');
+                else
+                    console.log('--BACKGROUND NOTIFICATION--' + '');
 
-//                // direct user here:
-//                window.location = "#/playlists";
-                
+                // direct user here:
+                window.location = "#/app/playlists";
+            }
 
-//            }
+            console.log('MESSAGE -> MSG: ' + e.payload.message + '');
+            console.log('MESSAGE: ' + JSON.stringify(e.payload));
+            break;
 
-//            console.log('MESSAGE -> MSG: ' + e.payload.message + '');
-//            console.log('MESSAGE: ' + JSON.stringify(e.payload));
-//            break;
+        case 'error':
+            console.log('ERROR -> MSG:' + e.msg + '');
+            break;
 
-//        case 'error':
-//            console.log('ERROR -> MSG:' + e.msg + '');
-//            break;
-
-//        default:
-//            console.log('EVENT -> Unknown, an event was received and we do not know what it is');
-//            break;
-//    }
-//}
+        default:
+            console.log('EVENT -> Unknown, an event was received and we do not know what it is');
+            break;
+    }
+}
